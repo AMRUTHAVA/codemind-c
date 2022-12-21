c630d6f862e043b62a5467bc996a56eaec1b6ea3
AMRUTHAVA <119877698+AMRUTHAVA@users.noreply.github.com>
2022-12-21 22:32:38 +0530

    #include<stdio.h> int main() {     int i,n,max=-1000;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(max<=a[i])         {             max=a[i];         }     }     printf("%d",max); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,max=-1000;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max<=a[i])
+        {
+            max=a[i];
+        }   
+    }
+    printf("%d",max);
+}
