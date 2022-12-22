22c8c8256a0b7f721bb85261d4a40da9d01e2026
AMRUTHAVA <119877698+AMRUTHAVA@users.noreply.github.com>
2022-12-22 21:50:42 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+}
