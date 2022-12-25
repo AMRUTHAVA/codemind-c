c046269ff62890193af511bc27218425bb67498a
AMRUTHAVA <119877698+AMRUTHAVA@users.noreply.github.com>
2022-12-25 10:55:12 +0530

    #include<stdio.h> int main() {     int a,n,r,con=0;     scanf("%d",&n);     for(a=2;a<n;a++)     {         r=n%a;         if(r==0)         {             con++;         }     }     if(con==0)     {         printf("prime");     }     else     {         printf("not a prime");     } }

---
+++ prime_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,r,con=0;
+    scanf("%d",&n);
+    for(a=2;a<n;a++)
+    {
+        r=n%a;
+        if(r==0)
+        {
+            con++;
+        }
+    }
+    if(con==0)
+    {
+        printf("prime");
+    }
+    else
+    {
+        printf("not a prime");
+    }
+}
