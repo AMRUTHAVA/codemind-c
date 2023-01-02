d420f2c0bd4800ad4184a3ce114f7732811feb36
AMRUTHAVA <119877698+AMRUTHAVA@users.noreply.github.com>
2023-01-02 10:14:13 +0530

    #include<stdio.h> int main() {     int n,i,flag=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(a[i]==k)         {             flag=1;             break;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,flag=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
