952db0ec3e9c12bae5856a4f3a3552ba38c2787c
AMRUTHAVA <119877698+AMRUTHAVA@users.noreply.github.com>
2023-01-11 12:05:44 +0530

    #include<stdio.h> int main() {     int ni,i;     scanf("%d",&ni);     for(i=1;i<=ni;i++)     {         int n,q,r,s=0;         scanf("%d",&n);         q=n;         while(q!=0)         {             r=q%10;             q=q/10;             s=s*10+r;         }         if(s==n)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int ni,i;
+    scanf("%d",&ni);
+    for(i=1;i<=ni;i++)
+    {
+        int n,q,r,s=0;
+        scanf("%d",&n);
+        q=n;
+        while(q!=0)
+        {
+            r=q%10;
+            q=q/10;
+            s=s*10+r;
+        }
+        if(s==n)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+}
